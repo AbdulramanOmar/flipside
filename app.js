69d26e06e4fbc310c76ab7bd78213e8c8d0f2376
AbdulramanOmar <168247524+AbdulramanOmar@users.noreply.github.com>
2025-01-29 05:59:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -98,7 +98,8 @@ app.get("/api/subscribers", async (req, res) => {
   }
 });
 
-// بدء السيرفر
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
